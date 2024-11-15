a6572a3d50bfef6e6852cb191172117edf7bce75
MUGESH MANOHARAN <samcharlesmugesh@gmail.com>
2024-11-15 16:30:17 +0530

    15/11/24
    cart page work in  progress

--- frontend/src/components/AddCartButton/AddCartButton.js
+++ frontend/src/components/AddCartButton/AddCartButton.js
@@ -1,47 +1,69 @@
 import { Button } from '@mui/material';
 import React, { useEffect, useState } from 'react'
+import { useDataContext } from '../../context/context';
 
-export const AddCartButton = (ProductName) => {
-   const [count, setCount] = useState(0);
+export const AddCartButton = ({ ProductName }) => {
+  const [count, setCount] = useState(0);
 
-   useEffect( () => {
-      console.log('Product Name : ', ProductName);
-   }, []);
+  const {cart, setCart, } = useDataContext();
 
-   const handleClick = () => {
-    console.log(`Add to Cart Button clicked : ${ProductName}` );
-   };
+  // -------------- Handle increment and Decrement 
+    function handleIncrement() {
+      setCount((prevCount) => {
+        const newCount = prevCount + 1;
+        return newCount;
+      });
+    }
 
-   return (
-     <Button
-       variant={count > 0 ? "contained" : "outlined"}
-       color="success"
-       className="m-0 p-0 ms-auto"
-       onClick={handleClick}
-     >
-       {count === 0 ? (
-         <span className="py-2 w-100" onClick={() => setCount(1)}>
-           ADD
-         </span>
-       ) : (
-         <div className="d-flex align-items-center w-100">
-           <span
-             style={{ cursor: "pointer", minWidth: "fit-content" }}
-             className="p-0 m-0 py-2 px-2 h-100 w-auto"
-             onClick={() => setCount(count - 1)}
-           >
-             -
-           </span>
-           <span className="mx-auto">{count}</span>
-           <span
-             style={{ cursor: "pointer", minWidth: "fit-content" }}
-             className=" p-0 m-0 py-2 px-2 h-10 w-auto"
-             onClick={() => setCount(count + 1)}
-           >
-             +
-           </span>
-         </div>
-       )}
-     </Button>
-   );
- };
+    function handleDecrement() {
+      setCount((prevCount) => {
+        const newCount = prevCount - 1;
+        return newCount;
+      });
+    }
+
+  const handleAddToCartButton = (props) => {
+    // console.log(`Product added to the cart : ${props.ProductName}`);
+    // console.log(`and the count value is , ${props.count}`);
+    setCart([...cart, { ProductName: props.ProductName, count: props.count }]);
+  };
+
+  useEffect(() => {
+    if (count > 0) {
+      handleAddToCartButton({ ProductName, count });
+      console.log('cart : ', cart);
+    }
+  }, [count, ProductName, handleAddToCartButton, cart]);
+
+  return (
+    <Button
+      variant={count > 0 ? "contained" : "outlined"}
+      color="success"
+      className="m-0 p-0 ms-auto"
+    >
+      {count === 0 ? (
+        <span className="py-2 w-100" onClick={() => handleIncrement()}>
+          ADD
+        </span>
+      ) : (
+        <div className="d-flex align-items-center w-100">
+          <span
+            style={{ cursor: "pointer", minWidth: "fit-content" }}
+            className="p-0 m-0 py-2 px-2 h-100 w-auto"
+            onClick={() => handleDecrement()}
+          >
+            -
+          </span>
+          <span className="mx-auto">{count}</span>
+          <span
+            style={{ cursor: "pointer", minWidth: "fit-content" }}
+            className=" p-0 m-0 py-2 px-2 h-10 w-auto"
+            onClick={() => handleIncrement()}
+          >
+            +
+          </span>
+        </div>
+      )}
+    </Button>
+  );
+};
